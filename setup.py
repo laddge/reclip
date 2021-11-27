d325cefe02d2a873dc20cd77c6544f3ac4211640
Laddge <dev.laddge@gmail.com>
2021-11-27 17:19:19 +0900

    [feat] enhance setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup
 from reclip import __version__
 
+try:
+    with open('README.md') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
 setup(
-    name="reclip",
+    name='reclip',
     version=__version__,
+    description='a clipboard server',
+    long_description=readme,
+    url='https://github.com/laddge/reclip',
+    author='Laddge',
+    author_email='dev.laddge@gmail.com',
+    licence='MIT',
     packages=['reclip'],
     entry_points={
-        "console_scripts": [
-            "reclip = reclip.__main__:main",
+        'console_scripts': [
+            'reclip = reclip.__main__:main',
         ],
     }
 )
