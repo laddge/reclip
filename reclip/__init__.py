597cc643763bde62832a52d7efda33976256a670
Laddge <dev.laddge@gmail.com>
2021-11-27 11:48:52 +0900

    [feat] add __main__.py

--- reclip/__init__.py
+++ reclip/__init__.py
@@ -1,4 +1,4 @@
 from .server import serve
 from .client import set, get, delete
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
---
+++ reclip/__main__.py
@@ -0,0 +1,54 @@
+import argparse
+from . import serve, set, get, delete
+
+
+def cmd_serve(args):
+    serve(host=args.host, port=args.port)
+
+
+def cmd_set(args):
+    set(host=args.host, port=args.port)
+
+
+def cmd_get(args):
+    get(host=args.host, port=args.port)
+
+
+def cmd_delete(args):
+    delete(host=args.host, port=args.port)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='reclip - a clipboard server')
+    subparsers = parser.add_subparsers()
+
+    parser_serve = subparsers.add_parser('serve', help='see `serve -h`')
+    parser_serve.add_argument('--host', default='127.0.0.1', help='host addr')
+    parser_serve.add_argument('--port', default=8000, help='port')
+    parser_serve.set_defaults(fn=cmd_serve)
+
+    parser_set = subparsers.add_parser('set', help='see `set -h`')
+    parser_set.add_argument('--host', default='127.0.0.1', help='host addr')
+    parser_set.add_argument('--port', default=8000, help='port')
+    parser_set.add_argument('content', help='content')
+    parser_set.set_defaults(fn=cmd_set)
+
+    parser_get = subparsers.add_parser('get', help='see `get -h`')
+    parser_get.add_argument('--host', default='127.0.0.1', help='host addr')
+    parser_get.add_argument('--port', default=8000, help='port')
+    parser_get.set_defaults(fn=cmd_get)
+
+    parser_delete = subparsers.add_parser('delete', help='see `delete -h`')
+    parser_delete.add_argument('--host', default='127.0.0.1', help='host addr')
+    parser_delete.add_argument('--port', default=8000, help='port')
+    parser_delete.set_defaults(fn=cmd_delete)
+
+    args = parser.parse_args()
+    if hasattr(args, 'fn'):
+        args.fn(args)
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
