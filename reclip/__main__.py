e223c0eb4c005cd69d94920ce3cbf03e7a03afab
Laddge <dev.laddge@gmail.com>
2021-11-27 11:59:11 +0900

    [feat] add default arg as comment to arg parser

--- reclip/__main__.py
+++ reclip/__main__.py
@@ -23,24 +23,24 @@ def main():
     subparsers = parser.add_subparsers()
 
     parser_serve = subparsers.add_parser('serve', help='see `serve -h`')
-    parser_serve.add_argument('--host', default='127.0.0.1', help='host addr')
-    parser_serve.add_argument('--port', default=8000, help='port')
+    parser_serve.add_argument('--host', default='127.0.0.1', help='host addr (default = \'127.0.0.1\')')
+    parser_serve.add_argument('--port', default=8000, help='port (default = 8000)')
     parser_serve.set_defaults(fn=cmd_serve)
 
     parser_set = subparsers.add_parser('set', help='see `set -h`')
-    parser_set.add_argument('--host', default='127.0.0.1', help='host addr')
-    parser_set.add_argument('--port', default=8000, help='port')
+    parser_set.add_argument('--host', default='127.0.0.1', help='host addr (default = \'127.0.0.1\')')
+    parser_set.add_argument('--port', default=8000, help='port (default = 8000)')
     parser_set.add_argument('content', help='content')
     parser_set.set_defaults(fn=cmd_set)
 
     parser_get = subparsers.add_parser('get', help='see `get -h`')
-    parser_get.add_argument('--host', default='127.0.0.1', help='host addr')
-    parser_get.add_argument('--port', default=8000, help='port')
+    parser_get.add_argument('--host', default='127.0.0.1', help='host addr (default = \'127.0.0.1\')')
+    parser_get.add_argument('--port', default=8000, help='port (default = 8000)')
     parser_get.set_defaults(fn=cmd_get)
 
     parser_delete = subparsers.add_parser('delete', help='see `delete -h`')
-    parser_delete.add_argument('--host', default='127.0.0.1', help='host addr')
-    parser_delete.add_argument('--port', default=8000, help='port')
+    parser_delete.add_argument('--host', default='127.0.0.1', help='host addr (default = \'127.0.0.1\')')
+    parser_delete.add_argument('--port', default=8000, help='port (default = 8000)')
     parser_delete.set_defaults(fn=cmd_delete)
 
     args = parser.parse_args()
